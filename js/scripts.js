2a884566862f67df925d9bb6bcf80ec9868d299b
iwarui722 <warui.r.s@gmail.com>
2020-03-03 09:34:11 +0300

    add functions

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+    let dayOutcomeInDom = document.querySelector('#dayOfWeek')
+    let akanOutcomeInDom = document.querySelector('#akanName');
+    
+    // Formulae to calculate day of Week Takes in century,year,month,and day parameters.
+    // March = 1, December = 10 January = 11, February 12;
+    let dayCalculator = (c,y,mon,d)=>{
+        let m;
+        if(mon>=3){
+            m = mon-2;
+        }else if(mon == 1){
+            m = 11
+        }else{
+            m = 12
+        }
+    
+        return Math.abs((d+ Math.floor((2.6*m)-0.2)-(2*c)+y+Math.floor(y/4)+Math.floor(c/4))%7);
+    }
